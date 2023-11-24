4885e9d87a61fad5ecb178706870f43338385102
Benjamin Day <69356549+BenjaminDay@users.noreply.github.com>
2023-11-24 02:18:43 +0000

    Update json_reader.py

--- json_reader.py
+++ json_reader.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import json
+with open('AirdropSettings.json', 'r') as file:
+    data = json.load(file)
+
+for container in data['Containers']:
+    for item in container['Loot']:
+        item['Chance'] = round(item['Chance'], 2)
+
+with open('your_file.json', 'w') as file:
+    json.dump(data, file, indent=2)
